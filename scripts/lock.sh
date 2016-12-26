4fb36801898f9705532fdf37eb7842ac24d12183
Sérgio Oliveira <sergiooliveira28@hotmail.com>
2016-12-26 21:15:57 +0000

    Desktops mudados

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -8,9 +8,9 @@ TEXT=/tmp/locktext.png
 ICON=/home/sergio/Imagens/Icons/icon2.png
  
 scrot $IMAGE
-convert $IMAGE -scale 10% -scale 1000% -fill black -colorize 25% $IMAGE
+convert $IMAGE -scale 10% -scale 1000% -fill black -colorize 24% $IMAGE
 [ -f $TEXT ] || {
-    convert -size 3000x60 xc:white -font Liberation-Sans -pointsize 26 -fill black -gravity center -annotate +0+0 '[Desbloquear] senha para sergio: ' $TEXT;
+    convert -size 3000x60 xc:white -font Liberation-Sans -pointsize 26 -fill black -gravity center -annotate +0+0 'sergio@K56CB [Bloqueado]' $TEXT;
     convert $TEXT -alpha set -channel A -evaluate set 50% $TEXT;
 }
 convert $IMAGE $TEXT -gravity center -geometry +0+200 -composite $IMAGE
